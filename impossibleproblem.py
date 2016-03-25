e7c3314223125c6566dad9dfeaae124a549290dc
David Zaslavsky <diazona@ellipsix.net>
2016-03-26 03:47:31 +0800

    Iterate over permutations instead of all n-digit numbers
    One way to avoid taking a lot of time checking whether all digits are used the right number of times is to start with the right set of digits and iterate over all permutations of it.
    Python has a built-in tool for that, the permutations() function of the itertools module.
    It takes a sequence like '123456789' and goes through all possible orderings of the elements in the sequence.
    All you have to do for each ordering is covert the first three digits into a number, the second three digits into a number, and the third three digits into a number, and see if the first number minus the second equals the third.
    To avoid giving duplicates, you can only test the case where the three numbers are produced in decreasing order.
    
    This slightly improves on the initial results.
    The profiler gives this output:
    
             1451702 function calls in 0.999 seconds
    
       Ordered by: standard name
    
       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
            1    0.000    0.000    0.999    0.999 <string>:1(<module>)
            1    0.000    0.000    0.999    0.999 impossibleproblem.py:16(main)
            1    0.626    0.626    0.999    0.999 impossibleproblem.py:4(find_solutions)
            1    0.000    0.000    0.999    0.999 {built-in method exec}
       362880    0.026    0.000    0.026    0.000 {built-in method iter}
            3    0.000    0.000    0.000    0.000 {built-in method len}
            4    0.000    0.000    0.000    0.000 {built-in method print}
            1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
          168    0.000    0.000    0.000    0.000 {method 'extend' of 'list' objects}
            2    0.000    0.000    0.000    0.000 {method 'format' of 'str' objects}
      1088640    0.347    0.000    0.347    0.000 {method 'join' of 'str' objects}

--- impossibleproblem.py
+++ impossibleproblem.py
@@ -1,36 +1,16 @@
+from itertools import islice, permutations
 from math import trunc
 
-def check_for_all_digits(a, b, n):
-    '''Checks whether a and b and their difference, taken together, include all
-    digits from 1 to 9 exactly n times each.'''
-    seq = str(a), str(b), str(a - b)
-    digits = "".join(seq)
-    goodChecks = 0
-    for i in range(1,10):
-        if digits.count(str(i)) == n:
-            goodChecks += 1
-    if goodChecks == 9:
-        return digits
-    else:
-        return None
-
 def find_solutions(num_of_digits):
-    if num_of_digits == 3:
-        minY = 381
-        maxY = 987
-        minX = 123
-    elif num_of_digits == 6:
-        minY = 246912
-        maxY = 998877
-        minX = 123123
-    else:
-        raise ValueError('invalid number of digits: {} should be 3 or 6'.format(num_of_digits))
+    digits = '123456789' * int(num_of_digits / 3)
     solutions = []
-    for y in range(minY, maxY+1):
-        for x in range(minX, trunc(y/2)):
-            digits = check_for_all_digits(y, x, num_of_digits / 3)
-            if digits is not None:
-                solutions.extend([x, y-x])
+    for p in permutations(digits):
+        i = iter(p)
+        a = int(''.join(islice(i, num_of_digits)))
+        b = int(''.join(islice(i, num_of_digits)))
+        c = int(''.join(islice(i, num_of_digits)))
+        if a > b > c and a - b == c:
+            solutions.extend([b, c])
     return solutions
 
 def main():
