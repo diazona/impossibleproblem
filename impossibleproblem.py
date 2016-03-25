5c8f442ced988764dce22ca7a518ecd2546112d0
David Zaslavsky <diazona@ellipsix.net>
2016-03-26 01:50:36 +0800

    Just fixing a few spacing issues
    Python has a standard, PEP 8, for how code should be spaced.
    It's not required, but it does tend to make code look pretty good and easy to read.
    Since readability of the source code in Python is important (it works as documentation), it's good to get used to the conventions.

--- impossibleproblem.py
+++ impossibleproblem.py
@@ -3,7 +3,7 @@ from math import trunc
 def check_for_all_digits(a, b, n):
     '''Checks whether a and b and their difference, taken together, include all
     digits from 1 to 9 exactly n times each.'''
-    seq = str(a),str(b), str(a - b)
+    seq = str(a), str(b), str(a - b)
     digits = "".join(seq)
     goodChecks = 0
     for i in range(1,10):
@@ -18,9 +18,9 @@ def find_solutions(minX, minY, maxY, num_of_digits):
     solutions = []
     successes = 0
     for y in range(minY, maxY+1):
-        if y%100 == 0:
+        if y % 100 == 0:
             print(y)
-        for x in range(minX,trunc(y/2)):
+        for x in range(minX, trunc(y/2)):
             digits = check_for_all_digits(y, x, num_of_digits / 3)
             if digits is not None:
                 successes += 2
@@ -35,16 +35,16 @@ def main():
         maxY = 987
         minX = 123
     elif num_of_digits == 6:
-        minY =246912
+        minY = 246912
         maxY = 998877
-        minX =123123
+        minX = 123123
     else:
         print("Not a valid number of digits!")
         return
 
     solutions, successes = find_solutions(minX, minY, maxY, num_of_digits)
 
-    print("")
+    print()
     print("I found: ", successes, " successful solution to your brainteaser")
     if successes < 20:
         print("there were almost no solutions")
